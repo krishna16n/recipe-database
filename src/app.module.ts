d157b359c7f72e1381ea5a81dbca256ae2455ca0
Krishna Sharma <76518983+krishna16n@users.noreply.github.com>
2024-03-13 17:24:05 +0530

    prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,34 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [],
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false
-      }
-      }),
-      inject: [ConfigService],
-    }),
-    AuthModule,
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'postgres',
+    //     host: configService.get('DB_HOST'),
+    //     port: +configService.get<number>('DB_PORT'),
+    //     username: configService.get('DB_USERNAME'),
+    //     password: configService.get('DB_PASSWORD'),
+    //     database: configService.get('DB_NAME'),
+    //     entities: [],
+    //     synchronize: true,
+    //     ssl: {
+    //       rejectUnauthorized: false
+    //   }
+    //   }),
+    //   inject: [ConfigService],
+    // }),
     UsersModule,
     RecipesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async signup() {
+        
+        return { message: 'success' }
+    }
+
+    async signin() {
+        return { message: 'success' }
+
+    }
+
+    async signout() {
+        return { message: 'success' }
+
+    }
+}
