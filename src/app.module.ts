c0e7e9f8550b8d6fa71da7c535857cbcbda486ba
Krishna Sharma <76518983+krishna16n@users.noreply.github.com>
2024-03-13 14:13:12 +0530

    postgres setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false
+      }
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
     UsersModule,
     RecipesModule,
