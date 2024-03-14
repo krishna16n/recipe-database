94f1c360fd82205b3798cb3293ebaeb8c4855b7a
Krishna Sharma <76518983+krishna16n@users.noreply.github.com>
2024-03-14 17:31:20 +0530

    refactor and docs

--- src/modules/recipes/recipes.controller.ts
+++ src/modules/recipes/recipes.controller.ts
@@ -3,31 +3,58 @@ import { RecipesService } from './recipes.service';
 import { CreateRecipeDto, UpdateRecipeDto } from './dto/recipe.dto';
 import { JwtAuthGuard } from 'src/core/guards/jwt.guard';
 
+
 @UseGuards(JwtAuthGuard)
 @Controller('recipes')
 export class RecipesController {
   constructor(private readonly recipesService: RecipesService) { }
 
+  /**
+   * Creates a new recipe.
+   * @param dto - The data transfer object containing recipe information.
+   * @param req - The request object.
+   * @returns The newly created recipe.
+   */
   @Post('create')
   create(@Body() dto: CreateRecipeDto, @Req() req) {
     return this.recipesService.createRecipe(dto, req);
   }
 
+  /**
+   * Updates an existing recipe by its ID.
+   * @param params - Object containing the ID of the recipe to be updated.
+   * @param dto - The data transfer object containing updated recipe information.
+   * @returns The updated recipe.
+   */
   @Put(':id')
   async updateRecipe(@Param() params: { id: string }, @Body() dto: UpdateRecipeDto) {
     return this.recipesService.updateRecipe(params.id, dto);
   }
 
+  /**
+   * Retrieves all recipes.
+   * @returns An array of all recipes.
+   */
   @Get()
   async getAllRecipes() {
     return this.recipesService.getAllRecipes();
   }
 
+  /**
+   * Retrieves a recipe by its ID.
+   * @param params - Object containing the ID of the recipe to be retrieved.
+   * @returns The recipe with the specified ID.
+   */
   @Get(':id')
   getRecipe(@Param() params: { id: string }) {
     return this.recipesService.getRecipe(params.id);
   }
 
+  /**
+   * Deletes a recipe by its ID.
+   * @param params - Object containing the ID of the recipe to be deleted.
+   * @returns The deleted recipe.
+   */
   @Delete(':id')
   deleteRecipe(@Param() params: { id: string }) {
     return this.recipesService.deleteRecipe(params.id);
--- src/modules/recipes/recipes.service.ts
+++ src/modules/recipes/recipes.service.ts
@@ -3,11 +3,18 @@ import { CreateRecipeDto, UpdateRecipeDto } from './dto/recipe.dto';
 import { PrismaService } from 'prisma/prisma.service';
 import { Request } from 'express';
 
+
 @Injectable()
 export class RecipesService {
 
     constructor(private prisma: PrismaService) { }
 
+    /**
+     * Creates a new recipe.
+     * @param createRecipeDto - The data transfer object containing recipe information.
+     * @param req - The request object.
+     * @returns The newly created recipe.
+     */
     async createRecipe(createRecipeDto: CreateRecipeDto, req: Request) {
         const { title, description, ingredients, instructions, authorId } = createRecipeDto;
 
@@ -23,6 +30,13 @@ export class RecipesService {
         });
     }
 
+    /**
+     * Updates an existing recipe by its ID.
+     * @param recipeId - The ID of the recipe to be updated.
+     * @param dto - The data transfer object containing updated recipe information.
+     * @returns The updated recipe.
+     * @throws NotFoundException if the recipe with the provided ID is not found.
+     */
     async updateRecipe(recipeId: string, dto: UpdateRecipeDto) {
         const { title, description, ingredients, instructions } = dto;
 
@@ -36,11 +50,6 @@ export class RecipesService {
             throw new NotFoundException('Recipe not found');
         }
 
-        // Check if the user is the author of the recipe
-        // if (recipe.authorId !== userId) {
-        //     throw new NotFoundException('Recipe not found');
-        // }
-
         // Update the recipe data
         return this.prisma.recipe.update({
             where: { id: recipeId },
@@ -53,16 +62,30 @@ export class RecipesService {
         });
     }
 
+    /**
+     * Retrieves all recipes.
+     * @returns An array of all recipes.
+     */
     async getAllRecipes() {
         return this.prisma.recipe.findMany();
     }
 
+    /**
+     * Retrieves a recipe by its ID.
+     * @param id - The ID of the recipe to be retrieved.
+     * @returns The recipe with the specified ID.
+     */
     async getRecipe(id: string) {
         return this.prisma.recipe.findUnique({
             where: { id },
         });
     }
 
+    /**
+     * Deletes a recipe by its ID.
+     * @param id - The ID of the recipe to be deleted.
+     * @returns The deleted recipe.
+     */
     async deleteRecipe(id: string) {
         return this.prisma.recipe.delete({
             where: { id },
