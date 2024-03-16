4ca9f3d63c9487fad0da26c1d3cb3eb8c4dec63f
Krishna Sharma <76518983+krishna16n@users.noreply.github.com>
2024-03-16 18:49:55 +0530

    signin updated

--- recipe-frontend/src/app/app.module.ts
+++ recipe-frontend/src/app/app.module.ts
@@ -17,13 +17,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     LayoutComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatListModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [
     provideAnimationsAsync(),
